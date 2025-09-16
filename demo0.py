155095e8ccefcc3053e3a05b4cfef8ccd42bb70a
hani-mohammed <117700149+hani-mohammed@users.noreply.github.com>
2025-09-16 09:07:39 -0600

    First the commit

---
+++ demo0.py
@@ -0,0 +1,10 @@
+# Hey CPU, I want you to allocate piece of memory, and I am going
+# to give a name. This name is x, and inside this memory
+# I am going to put the value 3. But CPU, be careful
+# wheneve I mention the word x , I need you to bring me the value of it.
+# which is 3
+x = 3 
+
+# Hey CPU, please print the value of x, remember it is 3, please 
+# print it to the screen.
+print(x) # function call 
---
+++ demo1.py
@@ -0,0 +1,21 @@
+x = 3
+y = 4.5
+message = "hello"
+msg = 'hola'
+
+old = True
+young = False 
+print(msg)
+
+greeting_msg = "Hello world!"
+greetingmsg = "hola!"
+greetingMsg = "hello again?"
+print(greetingMsg)
+v = 3
+
+z = v==3
+print(z+1)
+z = x=3
+print(z+1)
+iudofudsfodsufaodsfudsofusdof= 3
+fdjfdhfhfjdsahjdshf_23241= 4.3
