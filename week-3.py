0d5dd7032ba4ce8450c5ccd62ac8f21a99790d8a
hani-mohammed <hani.mohammed@sait.ca>
2025-09-19 15:52:07 -0600

    Week 7

---
+++ week-3.py
@@ -0,0 +1,64 @@
+# statements we know so far??
+
+# assignment statement
+i = 3  # Asking to reserve memory of 4 bytes (this is the size of intger)
+        # and put the value 3 in it.
+f = 3.4 
+msg = "Hello" 
+message = 'Hola'
+condition = True 
+# a variable is a memory that has a size, 
+# a name, an address, a value, and a type
+
+#Another statement is a function call 
+#The usage of a function is called a function call
+
+print("Hello World!") #The function takes an argument 
+                    #and print the value of it to the screen
+print(3)
+print(f)
+print(condition)
+
+# Let's explore other functions.
+
+print(i, type(i))
+print(f, type(f))
+print(msg, type(msg))
+print(condition, type(condition))
+
+# a function call has a name, and (), and in between we have arguemtnst.
+
+
+
+y = 3+4 #this evalutes to 7, so I evaluate the exp and put it in y
+
+username = input("What is your name?") # This will evalute to a text
+
+print("Welcome",username)
+
+# The casting functions
+#implicit casting
+
+x = 3
+y = 4
+z = x/y # floor division 
+num = 14.5
+age = int(num) # casting function 
+print("Value of z is", z, ". Type of z is", type(z))
+print(age)
+y = 4
+yf = float(y)
+print(yf)
+text = str(y)
+#print(text + 7) error 
+
+text_as_num = int(text)
+print(text_as_num + 7)
+#Task: write a program that asks the user to input 
+# their name, and send a greeting message with the name
+
+# a function does something, 
+# but a function can also take input and output 
+
+
+
